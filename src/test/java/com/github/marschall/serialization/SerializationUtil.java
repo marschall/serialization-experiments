e797fa360c3403de4962963e679ee6f9fe91e2e0
Philippe Marschall <philippe.marschall@gmail.com>
2013-12-14 15:04:38 +0100

    optimize counting

--- src/test/java/com/github/marschall/serialization/SerializationUtil.java
+++ src/test/java/com/github/marschall/serialization/SerializationUtil.java
@@ -5,10 +5,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 final class SerializationUtil {
@@ -22,12 +21,11 @@ final class SerializationUtil {
   }
 
   static int serializedSize(Serializable object) throws IOException {
-    // TODO optimize
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    CountingOutputStream outputStream = new CountingOutputStream();
     try (ObjectOutputStream objectStream = new ObjectOutputStream(outputStream)) {
       objectStream.writeObject(object);
     }
-    return outputStream.size();
+    return outputStream.getCount();
   }
 
   static Object dersialize(byte[] data) throws ClassNotFoundException, IOException {
@@ -40,6 +38,40 @@ final class SerializationUtil {
   static byte[] serializeJackson(Object pojo) throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
     return objectMapper.writeValueAsBytes(pojo);
+  }
+  
+  static final class CountingOutputStream extends OutputStream {
+    
+    private int count;
+
+    CountingOutputStream() {
+      this.count = 0;
+    }
+    
+    int getCount() {
+      return this.count;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      this.count += 1;
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+      this.count += b.length;
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+      if (b == null) {
+        throw new NullPointerException();
+      } else if ((off < 0) || (off > b.length) || (len < 0) || ((off + len) > b.length) || ((off + len) < 0)) {
+        throw new IndexOutOfBoundsException();
+      }
+      this.count += len;
+    }
+
   }
 
 }
