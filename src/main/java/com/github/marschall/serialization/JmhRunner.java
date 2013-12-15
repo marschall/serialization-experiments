41168b6a50f91b02449379d5ddf7e70635f5fb22
Philippe Marschall <philippe.marschall@gmail.com>
2013-12-15 12:50:38 +0100

    set up jmh

---
+++ src/main/java/com/github/marschall/serialization/JmhRunner.java
@@ -0,0 +1,22 @@
+package com.github.marschall.serialization;
+
+import org.openjdk.jmh.Main;
+
+public class JmhRunner {
+
+  private static final String TEST = ".*" + SerializationPerformance.class.getSimpleName() + ".*"; 
+  
+  public static void main(String[] args) {
+      String[] arguments = new String[]{TEST,
+          "-i", "" + 10,
+          "-r", 5000 + "ms",
+          "-t", "" + 1,
+          "-w", "100000",
+//          "-wi", "3",
+//          "-l",
+//          "-v"
+      };
+      Main.main(arguments);
+  }
+
+}
